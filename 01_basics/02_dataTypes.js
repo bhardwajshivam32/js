140fecec0c81c08b1a02e81a7d263f41a4889f45
Shivam Bhardwaj <137266872+bhardwajshivam32@users.noreply.github.com>
2024-03-09 11:12:43 +0000

    something new

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3 +
+     3);  //code readability shoild be high
+    
+    console.log("Shivam")
+
+
+let name = "shivam";
+let age = 22;
+let isLoggedIn = false;
+let state
+
+// nember => 2 to  power  53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null);
