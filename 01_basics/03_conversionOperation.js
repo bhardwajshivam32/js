5824b9b6d49273b32a41db9e6a2e88580682fcd3
Shivam Bhardwaj <137266872+bhardwajshivam32@users.noreply.github.com>
2024-03-15 09:30:55 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "45adv";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "45" => 45
+// "45adc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//  "" => false
+// "shivam" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
